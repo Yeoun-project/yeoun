3a15d49c41b88bc416ba232deebba81e493a4790
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-02-27 16:02:11 +0900

    feat: add https, and cookie token

--- src/main/java/com/example/demo/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.example.demo.jwt;
 import com.example.demo.service.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -27,15 +30,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
+        String token = getTokenFromCookies(request);
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (token == null) {
             chain.doFilter(request, response);
             return;
         }
 
         try {
-            String token = authHeader.substring(7);
             String userId = jwtUtil.extractUserId(token);
 
             if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
@@ -56,5 +58,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             log.error("인증 실패!, {}", e.getLocalizedMessage());
             chain.doFilter(request, response);
         }
+    }
+
+    private String getTokenFromCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            Optional<Cookie> tokenCookie = Arrays.stream(cookies)
+                    .filter(cookie -> "accessToken".equals(cookie.getName()))
+                    .findFirst();
+            if (tokenCookie.isPresent()) {
+                return tokenCookie.get().getValue();
+            }
+        }
+        return null;
     }
 }
