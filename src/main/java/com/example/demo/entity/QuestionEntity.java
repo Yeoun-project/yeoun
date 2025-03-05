582ed97c2c31ec3fe1e6ea5e77b075c0e3d18b70
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-03-05 14:03:18 +0900

    feat: include comment list

--- src/main/java/com/example/demo/entity/QuestionEntity.java
+++ src/main/java/com/example/demo/entity/QuestionEntity.java
@@ -1,11 +1,19 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.util.Date;
+import java.util.List;
 
-@Entity(name = "question")
+@Entity
+@Table(name = "question")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class QuestionEntity {
 
     @Id
@@ -24,10 +32,20 @@ public class QuestionEntity {
     @ManyToOne
     private CategoryEntity category;
 
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CommentEntity> comments;
+
     @CreatedDate
-    private Date createdDateTime;
+    private final Date createdDateTime = new Date();
 
     @Column
     private Date deletedDateTime;
 
+    @Builder
+    public QuestionEntity(String content, int heart, UserEntity user, CategoryEntity category) {
+        this.content = content;
+        this.heart = heart;
+        this.user = user;
+        this.category = category;
+    }
 }
