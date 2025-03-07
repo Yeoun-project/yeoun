99200967461aad3b6f6e9bbcfcfcb5d278b396d2
kwonhee1 <lkh8033@gmail.com>
2025-03-07 14:54:12 +0900

    rebase delevop by update entity

--- backend/src/main/java/com/example/demo/service/CommentService.java
+++ backend/src/main/java/com/example/demo/service/CommentService.java
@@ -1,8 +1,9 @@
 package com.example.demo.service;
 
-<<<<<<< HEAD:backend/src/main/java/com/example/demo/service/CommentService.java
+import com.example.demo.dto.request.AddCommentRequestDto;
 import com.example.demo.entity.QuestionEntity;
 import com.example.demo.repository.CommentRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +13,6 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
-=======
-import com.example.demo.dto.request.AddCommentRequestDto;
-import java.util.List;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CommentService {
     public List<?> getAllComments(Long questionId){
 
     }
@@ -34,5 +28,4 @@ public class CommentService {
     public void deleteComment(Long commentId) {
 
     }
->>>>>>> 7bb2900 (front back repo 병합으로인한 remain작업 전 commit):src/main/java/com/example/demo/service/CommentService.java
 }
