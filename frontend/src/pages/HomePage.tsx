9bad6004ccbf319a0e93485440fe93bc72ff3e0b
Ajunmiiin <wjdals3249@naver.com>
2025-03-13 01:06:56 +0900

    design : Circle 컴포넌트 디자인 수치 조정

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import Circle from '../components/circle/Circle';
 const HomePage = () => {
   return (
     <section className="flex justify-center items-center">
-      <Circle>안녕하세요 여운입니다.</Circle>
+      <Circle>다시 태어난다면, 당신은 어떻게 살고싶나요?</Circle>
     </section>
   );
 };
