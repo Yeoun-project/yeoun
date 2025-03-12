c198e7fbea093a22cf896eaafe5fdbf17cb7f44d
Ajunmiiin <wjdals3249@naver.com>
2025-03-12 23:54:03 +0900

    feat : Home 화면에 Circle 추가

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
+import Circle from '../components/bubble/Circle';
+
 const HomePage = () => {
-  return <div>home</div>;
+  return (
+    <section className="flex justify-center items-center">
+      <Circle />
+    </section>
+  );
 };
 
 export default HomePage;
