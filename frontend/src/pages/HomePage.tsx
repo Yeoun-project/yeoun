007993f9754ea4a7a241cb6ef31b7de7803e4023
Ajunmiiin <wjdals3249@naver.com>
2025-03-13 01:09:37 +0900

    feat : Home 화면에 Circle 컴포넌트 추가

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,8 +2,8 @@ import Circle from '../components/circle/Circle';
 
 const HomePage = () => {
   return (
-    <section className="flex justify-center items-center">
-      <Circle>다시 태어난다면, 당신은 어떻게 살고싶나요?</Circle>
+    <section className="flex flex-col  gap-20 justify-center items-center">
+      <Circle></Circle> <Circle>다시 태어난다면, 당신은 어떻게 살고싶나요?</Circle>
     </section>
   );
 };
