17c676913301cf8ea56513ae97d5391143899128
Ajunmiiin <wjdals3249@naver.com>
2025-03-16 21:08:58 +0900

    chore : 테스트용 NavBar 주석

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,11 @@ import { Outlet } from 'react-router-dom';
 
 import Provider from './components/provider/index.tsx';
 
-import NavBar from './components/NavBar.tsx';
-
 function App() {
   return (
     <>
       <Provider>
-        <NavBar />
+        {/* <NavBar /> */}
         <Outlet />
       </Provider>
     </>
