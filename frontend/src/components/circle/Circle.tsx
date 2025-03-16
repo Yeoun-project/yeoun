6e406d0d4a7645240a7c0de391651bd4aabeb0bb
Ajunmiiin <wjdals3249@naver.com>
2025-03-16 21:12:40 +0900

    style : Circle 컴포넌트 내 tailwindCSS 포맷팅

--- frontend/src/components/circle/Circle.tsx
+++ frontend/src/components/circle/Circle.tsx
@@ -6,17 +6,17 @@ interface CircleProps {
 
 const Circle = ({ children }: CircleProps) => {
   return (
-    <div className="relative rounded-full bg-white min-w-[130px] min-h-[130px] max-w-[300px] size-auto inline-flex items-center justify-center">
-      <div className="aspect-square circle bg-circle-gradient">
-        <p className="max-w-[230px] text-xl p-4 items-center justify-center text-black drop-shadow-md text-center break-keep [text-shadow:_0px_0px_3px_rgb(255,255,255)]">
+    <div className="relative inline-flex size-auto min-h-[130px] max-w-[300px] min-w-[130px] items-center justify-center rounded-full bg-white">
+      <div className="circle bg-circle-gradient aspect-square">
+        <p className="text-blur max-w-[230px] items-center justify-center p-4 text-center text-xl break-keep text-black drop-shadow-md">
           {children}
         </p>
       </div>
       <div className="circle-layer-container animate-spin-first">
-        <div className="bg-circle-gradient circle-layer skew-4 scale-109 animate-pulse-second" />
+        <div className="bg-circle-gradient circle-layer animate-pulse-second scale-109 skew-4" />
       </div>
       <div className="circle-layer-container animate-spin-second">
-        <div className="bg-circle-gradient circle-layer animate-pulse scale-107 skew-3" />
+        <div className="bg-circle-gradient circle-layer scale-107 skew-3 animate-pulse" />
       </div>
       <div className="circle-layer-container animate-spin-third">
         <div className="bg-circle-gradient circle-layer scale-105 skew-2" />
