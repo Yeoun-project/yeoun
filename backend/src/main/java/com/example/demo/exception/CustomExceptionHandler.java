63db89498a1aee35b964afc0f8ec2bfe4bbf897f
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-03-18 10:25:10 +0900

    feat: CustomExceptionHandler 구체화
    NoResourceFoundException, HttpRequestMethodNotSupportedException가 404를 반환 하도록 수정하였습니다.

--- backend/src/main/java/com/example/demo/exception/CustomExceptionHandler.java
+++ backend/src/main/java/com/example/demo/exception/CustomExceptionHandler.java
@@ -4,9 +4,11 @@ import com.example.demo.common.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -35,6 +37,18 @@ public class CustomExceptionHandler {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(new ErrorResponse("INVALID_PARAMETER", "파라미터 값이 잘못되었습니다"));
         }
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException e) {
+        return ResponseEntity.status(ErrorCode.NOT_FOUND.getHttpStatusCode())
+                .body(new ErrorResponse(ErrorCode.NOT_FOUND.getCode(), "요청 URL을 찾을 수 없습니다"));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity.status(ErrorCode.NOT_FOUND.getHttpStatusCode())
+                .body(new ErrorResponse(ErrorCode.NOT_FOUND.getCode(), "요청 URL에 대한 HTTP 메소드가 올바르지 않습니다"));
     }
 
     @ExceptionHandler(Exception.class) // 정의해두지 않은 Exception 은 일단 500으로 처리
