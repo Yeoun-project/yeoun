d98e9f25e472803c22ba6da92a997f968e456dde
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-03-25 11:17:59 +0900

    refactor: 폴더 구조 Clean Architecture로 변경

--- backend/src/main/java/yeoun/YeounApplication.java
+++ backend/src/main/java/yeoun/YeounApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class DemoApplication {
+public class YeounApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(YeounApplication.class, args);
 	}
 
 }
