28d18d8b14c15a26b602e23b617344a50e206a65
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-03-26 16:10:00 +0900

    test: CI/CD 배포 테스트

--- backend/src/test/java/yeoun/YeounApplicationTests.java
+++ backend/src/test/java/yeoun/YeounApplicationTests.java
@@ -3,7 +3,7 @@ package yeoun;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = YeounApplicationTests.class)
 class YeounApplicationTests {
 
 	@Test
