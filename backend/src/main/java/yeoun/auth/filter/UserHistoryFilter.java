50d60e90a5cccdecdd13b957821320a36e331c27
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-03-26 10:26:00 +0900

    style: edit code style

--- backend/src/main/java/yeoun/auth/filter/UserHistoryFilter.java
+++ backend/src/main/java/yeoun/auth/filter/UserHistoryFilter.java
@@ -8,7 +8,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -24,22 +26,20 @@ public class UserHistoryFilter extends OncePerRequestFilter {
     private final UserHistoryService userHistoryService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-        FilterChain filterChain) throws ServletException, IOException {
-
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 모든 접근 client가 Anonymous로 인한 authentication을 가지고 있어야 함
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication == null) {
+        if (authentication == null) {
             throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR, "UserHistoryFilter :: no authentication error"); // 로직상 실행되어서는 안되는 exception발생
         }
 
-        Long userId = (Long)authentication.getPrincipal();
+        Long userId = (Long) authentication.getPrincipal();
 
-        if(userId == null) {
+        if (userId == null) {
             throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR, "UserHistoryFilter :: user_id is null error");
         }
 
-        userHistoryService.addHistory(userId, JwtService.getIpFromRequest(request), request.getHeader("User-Agent"), request.getRequestURI());
+//        userHistoryService.addHistory(userId, JwtService.getIpFromRequest(request), request.getHeader("User-Agent"), request.getRequestURI());
 
         filterChain.doFilter(request, response);
     }
