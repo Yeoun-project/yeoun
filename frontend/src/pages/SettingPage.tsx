0b6518c82572fc9027092977769c9f1fff017960
dnaeuuon <codnjs842@naver.com>
2025-04-07 22:33:45 +0900

    style: 코드 스타일 변경

--- frontend/src/pages/SettingPage.tsx
+++ frontend/src/pages/SettingPage.tsx
@@ -3,18 +3,14 @@ import TabNav from "../components/TabNav";
 const SettingPage = () => {
   return (
     <>
-      <header className="-z-1 flex flex-col items-center justify-between p-6">
-          <div className="w-full flex items-center justify-center relative">
-            {/* Header */}
-            <div className="absolute left-0">
-              <BackArrow />
-            </div>
-            <div className="text-center">
-              <p>설정</p>
-            </div>
+      <main className="flex min-h-[100svh] flex-col gap-4">
+        {/* Header */}
+        <header className="relative flex justify-center p-6">
+          <div className="absolute top-6 left-6">
+            <BackArrow />
           </div>
-      </header>
-      <main className="-z-1 flex min-h-[90svh] flex-col items-center justify-between pb-[70px]">
+          <h3 className="w-full text-center">답변목록</h3>
+        </header>
         <ul className="w-full font-desc">
           <li className="px-6 py-4.5 border-b border-gray-400">로그아웃</li>
           <li className="px-6 py-4.5 border-b border-gray-400">알림</li>
