2090ee6e270841c7cba6a0b23be49d204a7f5157
Ajunmiiin <wjdals3249@naver.com>
2025-04-08 18:47:31 +0900

    feat : 오늘의 질문 비회원/회원 URL 판별 함수 정의

--- frontend/src/services/api/question/todayQuestion.ts
+++ frontend/src/services/api/question/todayQuestion.ts
@@ -1,17 +1,27 @@
-import { TodayQuestion } from '../../../type/question';
-import Response from '../../../type/response';
 import client from '../client';
 
-const todayQuestionUrl = '/public/question/today';
+import { TodayQuestion } from '../../../type/question';
+import UserType from '../../../type/auth/UserType';
+import Response from '../../../type/response';
 
-export const getTodayQuestion = async (): Promise<TodayQuestion> => {
-  const response = await client.get<Promise<Response<TodayQuestion>>>(todayQuestionUrl);
+const getTodayQuestionUrl = (type: UserType) => {
+  if (type === 'User') {
+    return '/api/question/today';
+  } else {
+    return '/public/question/today';
+  }
+};
+
+export const getTodayQuestion = async (userType: UserType): Promise<TodayQuestion> => {
+  const response = await client.get<Promise<Response<TodayQuestion>>>(
+    getTodayQuestionUrl(userType)
+  );
 
   return (await response.data).data;
 };
 
-export const addTodayQuestionComment = async (content: string) => {
-  const response = await client.post(todayQuestionUrl, { content });
+export const addTodayQuestionComment = async (userType: UserType, content: string) => {
+  const response = await client.post(getTodayQuestionUrl(userType), { content });
 
   return response;
 };
