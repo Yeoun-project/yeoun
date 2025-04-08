1a39faebce82c9d514ab5eb530a3a8bae7bb8b22
dnaeuuon <codnjs842@naver.com>
2025-04-09 00:42:54 +0900

    feat: 설정 페이지 UI 구현

--- frontend/src/pages/SettingPage.tsx
+++ frontend/src/pages/SettingPage.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 import BackArrow from "../components/common/BackArrow";
 import TabNav from "../components/TabNav";
+
 const SettingPage = () => {
+  const [activate, setActivate] = useState(true);
+
   return (
     <>
       <main className="flex min-h-[100svh] flex-col gap-4">
@@ -12,10 +17,46 @@ const SettingPage = () => {
           <h3 className="w-full text-center">답변목록</h3>
         </header>
         <ul className="w-full font-desc">
-          <li className="px-6 py-4.5 border-b border-gray-400">로그아웃</li>
-          <li className="px-6 py-4.5 border-b border-gray-400">알림</li>
-          <li className="px-6 py-4.5 border-b border-gray-400">버전정보</li>
-          <li className="px-6 py-4.5 border-b border-gray-400">문의하기</li>
+          <li
+            onClick={() => {}}
+            className="border-b border-[#AAAAAA] cursor-pointer overflow-hidden"
+          >
+            <div className="px-6 py-4.5 transition-transform duration-150 active:shadow-inner">
+              로그아웃
+            </div>
+          </li>
+
+          <li className="flex flex-row justify-between items-center border-b border-[#AAAAAA]">
+            <div className="px-6 py-4.5">
+              알림
+            </div>
+            <div className="px-6 py-4">
+              <button 
+                className={`w-[40px] h-[24px] p-[4px] flex items-center rounded-full transition-colors duration-300 
+                ${activate ? "bg-[#FC90D1]" : "bg-[#F5F5F5] border border-[#D4D4D4]"}`}
+                onClick={() => { setActivate(!activate) }}
+              >
+                <div
+                  className={`w-[16px] h-[16px]  flex items-center rounded-full bg-white shadow-md transform transition-transform duration-300 
+                    ${activate ? "translate-x-[16px] bg-[url(/icons/check.svg)] bg-no-repeat bg-center" : "translate-x-0 bg-[url(/icons/Union.svg)] bg-no-repeat bg-center"}`}
+                >
+                </div>
+              </button>
+            </div>
+          </li>
+          <li className="flex flex-row justify-between px-6 py-4.5 border-b border-[#AAAAAA]">
+            <div>
+              버전정보
+            </div>
+            <div>
+              1.0.0
+            </div>
+          </li>
+          <li className="border-b border-[#AAAAAA] cursor-pointer">
+            <div className="px-6 py-4.5 transition-transform duration-150 active:shadow-inner">
+              문의하기
+            </div>
+          </li>
         </ul>
       </main>
       <TabNav />
