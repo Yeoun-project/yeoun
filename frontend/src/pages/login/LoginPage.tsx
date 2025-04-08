59f025ee4f61b24a8fdad0935c7928aa2e22ef3a
Ajunmiiin <wjdals3249@naver.com>
2025-04-08 18:52:07 +0900

    chore : pages/login 폴더로 이동

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,7 +1,25 @@
-import { Link } from 'react-router-dom';
-import Circle from '../components/circle/Circle';
+import { Link, useNavigate } from 'react-router-dom';
+import Circle from '../../components/circle/Circle';
+import useAuthStore from '../../store/useAuthStore';
+import { useEffect } from 'react';
 
 const LoginPage = () => {
+  const { setUserType, userType } = useAuthStore();
+  const navigate = useNavigate();
+
+  // 비회원 로그인
+  const handleGuestLogin = () => {
+    setUserType('Guest');
+    navigate('/today-question');
+  };
+
+  useEffect(() => {
+    // 로그인 상태 일 때 리다이렉트
+    if (userType === 'User') {
+      navigate(-1);
+      return;
+    }
+  }, []);
   return (
     <main className="flex h-[100svh] flex-col items-center justify-between px-6 pt-10 pb-5">
       <div className="">
@@ -9,7 +27,7 @@ const LoginPage = () => {
           <Circle size={150} animate />
         </div>
         <p className="text-gradient text-center">
-          하루 한마디, 내 안에 스며드는
+          질문 하나, 마음속에 남는
           <span className="text-blur-pink pl-0.5 text-white"> 여운</span>
         </p>
       </div>
@@ -17,12 +35,12 @@ const LoginPage = () => {
       {/* Login Btn */}
       <ul className="font-desc flex w-full flex-col gap-4 text-center text-sm">
         <li>
-          <Link
-            to={'/today-question'}
-            className="block w-full rounded-xl border bg-transparent py-4.5"
+          <button
+            onClick={handleGuestLogin}
+            className="block w-full cursor-pointer rounded-xl border bg-transparent py-4.5"
           >
             로그인 없이 이용하기
-          </Link>
+          </button>
         </li>
         <li>
           <Link
