2d286c2b4a0e63e893122685b22f2ead9edebfdc
wheon06 <up2kid@gmail.com>
2025-04-09 20:30:50 +0900

    fix: cors 오류 수정

--- backend/src/main/java/yeoun/common/WebConfig.java
+++ backend/src/main/java/yeoun/common/WebConfig.java
@@ -11,15 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PATCH.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(),
-                        HttpMethod.OPTIONS.name()
-                )
+                .allowedOrigins("https://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
 
