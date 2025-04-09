f2976020172937aec079a3d283bac22c441d6342
wheon06 <up2kid@gmail.com>
2025-04-09 20:23:36 +0900

    fix: cors 오류 수정

--- backend/src/main/java/yeoun/common/WebConfig.java
+++ backend/src/main/java/yeoun/common/WebConfig.java
@@ -1,6 +1,7 @@
 package yeoun.common;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,9 +11,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://localhost:5173", "https://*.yeoun.kr")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
                 .allowCredentials(true);
     }
 
