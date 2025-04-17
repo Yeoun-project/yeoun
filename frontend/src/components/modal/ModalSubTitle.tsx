66a56bee9dda99999f3e02c6658d066c28a0fd1e
dnaeuuon <codnjs842@naver.com>
2025-04-17 17:36:31 +0900

    chore: modal style 수정

--- frontend/src/components/modal/ModalSubTitle.tsx
+++ frontend/src/components/modal/ModalSubTitle.tsx
@@ -1,5 +1,5 @@
 const ModalSubTitle = ({ children }: { children: React.ReactNode }) => {
-  return <p className="font-desc text-center text-sm text-[#6D6D6D]">{children}</p>;
+  return <p className="font-desc px-4 text-center text-sm text-[#6D6D6D]">{children}</p>;
 };
 
 export default ModalSubTitle;
