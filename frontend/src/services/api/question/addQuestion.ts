f81102c41a9b125b478e18ccd5768dea43c8be4d
dnaeuuon <codnjs842@naver.com>
2025-04-20 17:15:14 +0900

    feat: 금지어 탐색 api 추가

--- frontend/src/services/api/question/addQuestion.ts
+++ frontend/src/services/api/question/addQuestion.ts
@@ -1,5 +1,14 @@
 import client from "../client";
 
+export const verifiedQuestion = async (content: string, categoryId: number) => {
+    const response = await client.post('/api/question/forbidden-words', {
+        content: content,
+        categoryId: categoryId,
+    })
+
+    return response;
+}
+
 export const addUserQuestion = async (content: string, categoryId: number) => {
     const response = await client.post('/api/question', { 
         content : content, 
