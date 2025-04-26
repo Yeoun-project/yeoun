49c985e762a47d25d84e4cae775a7d42caf95fc5
Ajunmiiin <wjdals3249@naver.com>
2025-04-26 17:22:59 +0900

    feat : 질문 리스트 GET API 함수 정의

---
+++ frontend/src/services/api/question/getQuestions.ts
@@ -0,0 +1,64 @@
+import client from '../client';
+
+import CATEGORY from '../../../constant/category/Category';
+
+import Response from '../../../type/response';
+import { QuestionListReq } from '../../../type/questionListRequest';
+
+import QuestionCategory from '../../../type/questionCategory';
+import { QuestionList } from '../../../type/question';
+
+const getQuestionListApiUrl = (url: string, { page = 1, category }: QuestionListReq) => {
+  if (category) {
+    return `${url}?page=${page}&category=${category}`;
+  } else {
+    return `${url}?page=${page}`;
+  }
+};
+
+const getCategoryName = (categoryId: string | undefined) => {
+  if (!categoryId) return undefined;
+
+  const filteredCategory = CATEGORY.filter((category) => category.id === Number(categoryId))[0];
+
+  return filteredCategory.category;
+};
+
+// 내가 [작성한] 질문 리스트
+const getMyQuestions = async ({ page = 1, categoryId }: QuestionListReq): Promise<QuestionList> => {
+  const response = await client.get<Response<QuestionList>>(
+    getQuestionListApiUrl('/api/question/my', {
+      page,
+      category: getCategoryName(categoryId) as QuestionCategory,
+    })
+  );
+
+  return response.data.data;
+};
+
+// 내가 [답변한] 질문 리스트
+const getAnsweredQuestions = async ({
+  page = 1,
+  categoryId,
+}: QuestionListReq): Promise<QuestionList> => {
+  const response = await client.get<Response<QuestionList>>(
+    getQuestionListApiUrl('/api/question/commented-by-me', {
+      page,
+      category: getCategoryName(categoryId) as QuestionCategory,
+    })
+  );
+
+  return response.data.data;
+};
+
+// [오늘의 질문] 답변 리스트
+const getTodayAnswersQuestions = async ({ page = 1 }: QuestionListReq): Promise<QuestionList> => {
+  const response = await client.get<Response<QuestionList>>(
+    getQuestionListApiUrl('/public/today-question/my', {
+      page,
+    })
+  );
+
+  return response.data.data;
+};
+export { getMyQuestions, getAnsweredQuestions, getTodayAnswersQuestions };
