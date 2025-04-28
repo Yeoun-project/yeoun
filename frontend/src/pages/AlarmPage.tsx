20b3af9f333b5bc0d403347dada52bdae930eff5
dnaeuuon <codnjs842@naver.com>
2025-04-28 15:13:04 +0900

    feat: alarm page UI

--- frontend/src/pages/AlarmPage.tsx
+++ frontend/src/pages/AlarmPage.tsx
@@ -1,16 +1,30 @@
+import AlarmItem from '../components/alarmList/AlarmItem';
 import BackArrowButton from '../components/button/BackArrowButton';
 import BottomTabBar from '../components/nav/BottomTabBar';
-// import { getAlarmList, postTestAlarm } from '../services/api/alarm/alarmList';
+import FallBack from '../components/ui/FallBack';
+
+const list = [
+  {
+    questionId: 1,
+    content: '[today question 1]에 새로운 답변이 달렸어요!',
+    createTime: '2025-04-28T14:45:35.998',
+  },
+  {
+    questionId: 1,
+    content: '[today question 1]에 당신의 답변이 [남이권희]에게 여운을 남겼어요! ❤️',
+    createTime: '2025-04-28T16:59:47.136',
+  },
+  {
+    questionId: 2,
+    content: '[today question 2]에 당신의 답변이 2명 에게 여운을 남겼어요! ❤️',
+    createTime: '2025-04-28T17:00:01.725',
+  },
+];
 
 const AlarmPage = () => {
-  // const alarmList = getAlarmList(); 
-  // const test = postTestAlarm();
-  // console.log(test)
-  // console.log(alarmList);
-
   return (
     <>
-      <main className="flex min-h-[100svh] flex-col gap-4">
+      <main className="flex h-[100svh] flex-col">
         {/* Header */}
         <header className="relative flex justify-center p-6">
           <div className="absolute top-6 left-6">
@@ -18,7 +32,14 @@ const AlarmPage = () => {
           </div>
           <h3 className="w-full text-center">알람</h3>
         </header>
-        <ul className="font-desc w-full"></ul>
+        <div className="flex h-[calc(100%-140px)] flex-col">
+          {list.length === 0 && <FallBack desc="" subDesc="오늘은 조용한 하루였어요" />}
+          <ul className="font-desc w-full">
+            {list.map((item) => (
+              <AlarmItem id={item.questionId} content={item.content} createTime={item.createTime} />
+            ))}
+          </ul>
+        </div>
       </main>
       <BottomTabBar />
     </>
