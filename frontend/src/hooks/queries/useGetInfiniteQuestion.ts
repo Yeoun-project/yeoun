774b976b9921d387e811bc6cf74556dfcfa0d1a7
bye-on <codnjs842@naver.com>
2025-05-18 13:32:23 +0900

    chore: pageParam 수정

--- frontend/src/hooks/queries/useGetInfiniteQuestion.ts
+++ frontend/src/hooks/queries/useGetInfiniteQuestion.ts
@@ -21,7 +21,7 @@ const useGetInfiniteQuestion = <T>({
         page: pageParam as number,
         categoryId: categoryId === '0' ? undefined : (categoryId as string),
       }),
-    initialPageParam: 1,
+    initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) => (lastPage.hasNext ? allPages.length + 1 : undefined),
     select: (data) => {
       // 새로 불러온 데이터들이 있다면 기존 데이터들과 매핑 후 반환
