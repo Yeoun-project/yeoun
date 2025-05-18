6788af05dec472a014b67639b039fb4128e07e53
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-05-18 13:27:10 +0900

    refactor:  FETCH JOIN이랑 페이징을 함께 쓸 수 없는 부분 수정

--- backend/src/main/java/yeoun/question/domain/repository/QuestionRepository.java
+++ backend/src/main/java/yeoun/question/domain/repository/QuestionRepository.java
@@ -59,7 +59,7 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
             Pageable pageable
     );
 
-    @Query("select q from Question q left join fetch q.comments where q.user.id = :userId")
+    @Query("SELECT question FROM Question question WHERE question.user.id = :userId")
     Slice<Question> findByUserId(@Param("userId")Long userId, Pageable pageable);
 
     // 이전에 조회된 적 없는 인기 질문들 중 랜덤 1개의 질문 조회
