94c8d2c10738ca58b31feaba9d7a6d6531c50e12
dnaeuuon <codnjs842@naver.com>
2025-05-21 17:26:01 +0900

    fix: logout response type 변경

--- frontend/src/services/api/auth/logout.ts
+++ frontend/src/services/api/auth/logout.ts
@@ -1,7 +1,8 @@
 import client from '../client';
+import Response from '../../../type/response';
 
 export const logout = async () => {
-  const response = await client.get<Promise<Response>>('/api/user/logout');
+  const response = await client.get<Promise<Response<null>>>('/api/user/logout');
 
   return response;
 };
