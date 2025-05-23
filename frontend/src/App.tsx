cb3311a6a32f77e24648e05a5b7fad73e4dcd578
Ajungmiiin <wjdals3249@naver.com>
2025-05-23 23:52:12 +0900

    feat : 온보딩 페이지 연동

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
 
 import Provider from './components/provider/index.tsx';
+import OnboardingPage from './pages/OnboardingPage.tsx';
 
 function App() {
+  const hasComplete = localStorage.getItem('onboarding') === 'complete';
   return (
     <>
       <Provider>
         {/* <NavBar /> */}
-        <Outlet />
+        {hasComplete ? <Outlet /> : <OnboardingPage />}
       </Provider>
     </>
   );
