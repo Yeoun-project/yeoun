69807cd2c030b7f9f3c8c9dbcb972821ba942e86
Ajungmiiin <wjdals3249@naver.com>
2025-05-26 17:20:12 +0900

    design : 레이아웃 깨짐 수정

--- frontend/src/components/onboarding/OnBoardingStepOne.tsx
+++ frontend/src/components/onboarding/OnBoardingStepOne.tsx
@@ -7,7 +7,7 @@ const OnBoardingStepOne = () => {
         <p className="mb-2 text-2xl">여운은 어떤 공간인가요?</p>
         <p className="text-[#aaaaaa]">질문 하나, 마음속에 남는 여운</p>
       </div>
-      <div className="my-12 flex justify-center">
+      <div className="my-12 flex justify-center overflow-hidden p-2">
         <Circle size={260} animate>
           <p className="text-blur p-4 text-lg break-keep text-black">
             다시 태어난다면, 당신은 어떻게 살고싶나요?
