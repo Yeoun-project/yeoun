58e78b8e7ae6f7be344b5ddd71652a13009e19e6
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-05-28 09:48:08 +0900

    feat: timezone 설정

--- backend/src/main/java/yeoun/YeounApplication.java
+++ backend/src/main/java/yeoun/YeounApplication.java
@@ -1,8 +1,11 @@
 package yeoun;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,6 +13,12 @@ public class YeounApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YeounApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// timezone 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
