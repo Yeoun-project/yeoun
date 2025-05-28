62a2fb1091f729b0176c72e6d72a472bc9004781
Ajungmiiin <wjdals3249@naver.com>
2025-05-28 23:44:17 +0900

    feat : UTProvider 추가

--- frontend/src/components/provider/index.tsx
+++ frontend/src/components/provider/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import QueryProvider from './query';
 import ToastProvider from './toast';
+import UTProvder from './UTProvder';
+import { useIsMobile } from '../../hooks/useIsMobile';
 
 interface ProviderProps {
   children: React.ReactNode;
 }
 
 const Provider = ({ children }: ProviderProps) => {
+  const checkIsMobileView = useIsMobile();
+  const utReview = localStorage.getItem('utReview') === 'complete';
   return (
     <QueryProvider>
+      {checkIsMobileView && !utReview && <UTProvder />}
       <ToastProvider />
       {children}
     </QueryProvider>
