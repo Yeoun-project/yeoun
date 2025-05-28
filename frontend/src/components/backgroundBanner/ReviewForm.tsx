5a500a56e2343d04baa285fa81e8adfc60c12cfb
Ajungmiiin <wjdals3249@naver.com>
2025-05-28 23:44:04 +0900

    feat : API 연동

--- frontend/src/components/backgroundBanner/ReviewForm.tsx
+++ frontend/src/components/backgroundBanner/ReviewForm.tsx
@@ -1,16 +1,25 @@
 import { useState } from 'react';
 import Rating from './Rating';
+import writeUtReview from '../../services/api/writeUtReview';
 
 const ReviewForm = () => {
+  const completeReview = localStorage.getItem('utReview') === 'complete';
+
   const [currentRating, setCurrentRating] = useState<number>(0);
   const [review, setReview] = useState<string>('');
-  const [submitComplete, setSubmitComplete] = useState(false);
+  const [submitComplete, setSubmitComplete] = useState(completeReview);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setSubmitComplete(true);
-    setCurrentRating(0);
-    setReview('소중한 의견 남겨주셔서 감사합니다!');
+    try {
+      await writeUtReview({ starRate: currentRating, message: review });
+      setSubmitComplete(true);
+      setCurrentRating(0);
+      setReview('소중한 의견을 남겨주셔서 감사합니다!');
+      localStorage.setItem('utReview', 'complete');
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -29,7 +38,11 @@ const ReviewForm = () => {
           name="review"
           id="review"
           className="font-desc mb-2 h-[180px] w-full resize-none rounded-2xl border border-[#717171]/30 bg-white px-8 py-6 transition-colors outline-none disabled:text-[#EC69B8]"
-          placeholder="서비스를 사용하면서 불편했던 점이나 개선이 필요한 부분을 작성해주세요"
+          placeholder={
+            submitComplete
+              ? '소중한 의견 남겨주셔서 감사합니다!'
+              : '서비스를 사용하면서 불편했던 점이나 개선이 필요한 부분을 작성해주세요'
+          }
           value={review}
           onChange={(e) => setReview(e.target.value)}
         />
