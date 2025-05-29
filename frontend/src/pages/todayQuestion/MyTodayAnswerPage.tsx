32f18884665f4034bda447a53891227f10941332
Ajungmiiin <wjdals3249@naver.com>
2025-05-29 17:19:45 +0900

    chore : 모달 라이팅 수정

--- frontend/src/pages/todayQuestion/MyTodayAnswerPage.tsx
+++ frontend/src/pages/todayQuestion/MyTodayAnswerPage.tsx
@@ -81,7 +81,7 @@ const MyTodayAnswerPage = () => {
           <Modal.Header>
             <Modal.Title>등록하신 답변을 삭제하시겠습니까?</Modal.Title>
             <Modal.SubTitle>
-              삭제 후 답변 <span className="text-error">복구</span>는 불가능합니다.
+              삭제 후 답변 <span className="text-error">복구는 불가능합니다.</span>
             </Modal.SubTitle>
           </Modal.Header>
 
