ab08111faab74d16d11a0ef762a3a390f6554439
Ajungmiiin <wjdals3249@naver.com>
2025-06-01 20:09:16 +0900

    fix : 데스크탑 UT 리뷰 폼 계속 전송 할 수 있도록 수정

--- frontend/src/components/backgroundBanner/ReviewForm.tsx
+++ frontend/src/components/backgroundBanner/ReviewForm.tsx
@@ -3,11 +3,9 @@ import Rating from './Rating';
 import writeUtReview from '../../services/api/writeUtReview';
 
 const ReviewForm = () => {
-  const completeReview = localStorage.getItem('utReview') === 'complete';
-
   const [currentRating, setCurrentRating] = useState<number>(0);
   const [review, setReview] = useState<string>('');
-  const [submitComplete, setSubmitComplete] = useState(completeReview);
+  const [submitComplete, setSubmitComplete] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -16,7 +14,6 @@ const ReviewForm = () => {
       setSubmitComplete(true);
       setCurrentRating(0);
       setReview('소중한 의견을 남겨주셔서 감사합니다!');
-      localStorage.setItem('utReview', 'complete');
     } catch (error) {
       console.log(error);
     }
@@ -27,30 +24,29 @@ const ReviewForm = () => {
       <Rating
         currentRating={currentRating}
         setCurrentRating={(rating) => {
-          if (submitComplete) return;
           setCurrentRating(rating);
         }}
       />
 
       <div>
         <textarea
-          disabled={submitComplete}
           name="review"
           id="review"
-          className="font-desc mb-2 h-[180px] w-full resize-none rounded-2xl border border-[#717171]/30 bg-white px-8 py-6 transition-colors outline-none disabled:text-[#EC69B8]"
-          placeholder={
-            submitComplete
-              ? '소중한 의견 남겨주셔서 감사합니다!'
-              : '서비스를 사용하면서 불편했던 점이나 개선이 필요한 부분을 작성해주세요'
-          }
+          className={`font-desc mb-2 h-[180px] w-full resize-none rounded-2xl border border-[#717171]/30 bg-white px-8 py-6 transition-colors outline-none ${submitComplete ? 'text-[#EC69B8]' : 'text-[#1a1a1a]'}`}
+          placeholder="서비스를 사용하면서 불편했던 점이나 개선이 필요한 부분을 작성해주세요"
           value={review}
-          onChange={(e) => setReview(e.target.value)}
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+          onFocus={() => {
+            if (submitComplete) {
+              setSubmitComplete(false);
+              setReview('');
+            }
+          }}
         />
 
-        <button
-          disabled={review.trim().length === 0 || submitComplete}
-          className="font-desc w-full cursor-pointer rounded-lg bg-white py-3 transition-colors hover:bg-[#FC90D1] hover:text-white active:bg-[#EC69B8] active:text-white disabled:bg-[#DADADA] disabled:text-[#717171]"
-        >
+        <button className="font-desc w-full cursor-pointer rounded-lg bg-white py-3 transition-colors hover:bg-[#FC90D1] hover:text-white active:bg-[#EC69B8] active:text-white disabled:bg-[#DADADA] disabled:text-[#717171]">
           리뷰 등록하기
         </button>
       </div>
