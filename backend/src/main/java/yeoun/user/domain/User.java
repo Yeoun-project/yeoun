5161acebe2a7ba47c52d884b0e796f9271912f3c
kwonhee1 <lkh8033@gmail.com>
2025-06-01 23:20:08 +0900

    fix : seft delete

--- backend/src/main/java/yeoun/user/domain/User.java
+++ backend/src/main/java/yeoun/user/domain/User.java
@@ -24,7 +24,7 @@ import yeoun.question.domain.QuestionHistory;
 @Entity
 @Table(name = "user")
 @Getter
-@SQLDelete(sql = "UPDATE user SET delete_time = CURRENT_TIMESTAMP, uuid = 'deleted' , is_notification = false, email='deleted' WHERE id = ?") // soft delete
+@SQLDelete(sql = "UPDATE user SET delete_time = CURRENT_TIMESTAMP, o_auth_id = 'deleted', uuid = 'deleted' , is_notification = false, email='deleted' WHERE id = ?") // soft delete
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User implements UserDetails {
 
